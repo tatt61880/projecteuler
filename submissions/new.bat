c880140a3518642905509ce453d68df96a27fa91
tatt61880 <tatt61880@gmail.com>
2020-04-06 00:05:33 +0900

    Update new.bat

--- submissions/new.bat
+++ submissions/new.bat
@@ -1,16 +1,15 @@
 @echo off
 
-set INPUT=
 :RETRY
+set INPUT=
 set /p INPUT="”Ô†‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢: "
 if "%INPUT%"=="" (
 	goto :RETRY
 )
-if exist %INPUT% (
-	echo %INPUT% ‚ÍŠù‚É‘¶Ý‚µ‚Ü‚·B
-	goto :RETRY
+if not exist %INPUT% (
+  xcopy /S /I .\_template .\%INPUT% > nul
 )
 
-xcopy /S /I .\_template .\%INPUT% > nul
 start .\%INPUT%\main.kn
 start https://projecteuler.net/problem=%INPUT%
+
